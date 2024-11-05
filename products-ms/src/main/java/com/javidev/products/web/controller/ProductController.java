517ad84f497996ebd9dc41fdb70f733d0489aa9b
Javier A. Roque Sañudo <javicaib.mail@gmail.com>
2024-11-05 21:46:00 +0100

    add save controller

---
+++ products-ms/src/main/java/com/javidev/products/web/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.javidev.products.web.controller;
+
+import com.javidev.products.application.services.interfaces.ProductService;
+import com.javidev.products.web.dto.ProductCreateDTO;
+import com.javidev.products.web.dto.ProductResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductController {
+    final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> save(@RequestBody @Valid ProductCreateDTO productCreateDTO) {
+        return ResponseEntity.status(201).body(productService.save(productCreateDTO));
+    }
+}
